726deeb6708a88745a0553f64d81041062210ca4
joseluiseiguren <joseluiseiguren@gmail.com>
2019-10-26 21:34:27 +0200

    init

---
+++ DownloadFiles/Controllers/ValuesController.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace DownloadFiles.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly string FilesFolder = @"C:\temp\filestodownload";
+
+        [HttpGet("download/{file}")]
+        public async Task<IActionResult> Download(string file)
+        {
+            var filePath = Path.Combine(FilesFolder, file);
+            if (!System.IO.File.Exists(filePath))
+                return NotFound();
+
+            /*//open browser dialog
+            var memory = new MemoryStream();
+            using (var stream = new FileStream(filePath, FileMode.Open))
+            {
+                await stream.CopyToAsync(memory);
+            }
+            memory.Position = 0;
+
+            return File(memory, GetContentType(filePath), file);*/
+
+            //open directly in the browser
+            var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+
+            return File(fileStream, GetContentType(filePath));
+        }
+
+        private string GetContentType(string path)
+        {
+            var provider = new FileExtensionContentTypeProvider();
+            string contentType;
+            if (!provider.TryGetContentType(path, out contentType))
+            {
+                contentType = "application/octet-stream";
+            }
+            return contentType;
+        }
+    }
+}
